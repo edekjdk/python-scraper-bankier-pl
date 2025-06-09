6ccf0ae12d2cbecadbe0e0518d98b6fa3f9a2b92
Jakub Maternia <jmaternia@protonmail.com>
2025-06-09 16:26:48 +0200

    Alpha GUI

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from scraper.scraper import Scraper
 from scraper.config import create_driver
 from scraper.utils import save_to_csv
+from gui import StockApp
 from datetime import datetime, time
 import time as systime
-
+import sys
+from gui import StockApp
 
 def main():
+
     start_hour = time(9, 0)
     end_hour = time(17, 0)
 
@@ -24,21 +27,21 @@ def main():
             data1 = scraper.scrape_wig_20_main_table_data()
             data2 = scraper.scrape_each_wig20_company_data()
 
-            # print_scraped_data(data1)
-            # print_scraped_data(data2)
-
             save_to_csv(data1, "data/data1.csv")
             save_to_csv(data2, "data/data2.csv")
 
             driver.quit()
-            systime.sleep(1800)            
+            systime.sleep(1800)
         elif current_time < start_hour:
             systime.sleep(300)
-
         else:
             print("Dzisiejszego dnia sesja jest juz zamknieta")
             break
 
-
 if __name__ == "__main__":
-    main()
+    if "--gui" in sys.argv:
+        app = StockApp()
+        app.mainloop()
+    else:
+        main()
+
